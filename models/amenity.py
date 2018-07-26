9d67d0e160d8b1c2803dea96e3c24216a56bb994
vmdn23 <victormdnguyen88@gmail.com>
2018-07-26 01:58:59 +0000

    change ammenity syntax

--- models/amenity.py
+++ models/amenity.py
@@ -7,7 +7,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from os import getenv
 
-
 class Amenity(BaseModel, Base):
 
     '''
@@ -16,7 +15,8 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = "amenities"
 
-    name = Column(String(128), nullable=False)
-
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary="place_amenity")
+    else:
+        name = ""
