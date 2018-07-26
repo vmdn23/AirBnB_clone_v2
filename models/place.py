29c8eefc24b97a677cb9866df15e51c88c4faf85
vmdn23 <victormdnguyen88@gmail.com>
2018-07-26 03:11:56 +0000

    add primary key to place_amenity and back populate to place_amenities

--- models/place.py
+++ models/place.py
@@ -12,9 +12,11 @@ from models.amenity import Amenity
 
 place_amenity = Table("place_amenity", Base.metadata,
                       Column('place_id', String(60),
-                             ForeignKey("places.id"), nullable=False),
-                      Column('amenities_id', String(60),
-                             ForeignKey("amenities.id"), nullable=False))
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -37,10 +39,11 @@ class Place(BaseModel, Base):
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
-        reviews = relationship(Review, backref="place",
+        reviews = relationship("Review", backref="place",
                                cascade="all, delete-orphan")
-        amenities = relationship(Amenity, secondary=place_amenity,
-                                 viewonly=False)
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
 
     else:
         @property
