245d3532365fd7553fa6047277ed20f200115adc
vmdn23 <victormdnguyen88@gmail.com>
2018-07-31 02:59:14 +0000

    fix syntax, and switched to importing for api

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -15,16 +15,18 @@ def do_pack():
         Else, return None
 
     """
-    from fabric.operations import local
+    from fabric.api import local
     from datetime import datetime
 
-    time = datetime.now().strftime("%Y%m%d%H%M%S%f")
-    name = "./versions/web_static_{}.tgz".format(time)
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-
-    tar_file = local("tar -czfv {} web_static".format(name))
+    name = "./versions/web_static_{}.tgz".format(time)
+    tar_file = local("tar -cvzf {} web_static".format(name))
 
     if tar_file.succeeded:
         return name
     else:
         return None
+
+if __name__ == "__main__":
+    do_pack()
