452f76d72f20a6472511da6afb8c97b54a74510a
vmdn23 <victormdnguyen88@gmail.com>
2018-08-03 21:00:07 +0000

    pep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,22 +19,22 @@ env.key_filename = "~/.ssh/holberton"
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
-    Return: 
+    Return:
         False if the file at the path archive_path doesn’t exist
     """
     fp = archive_path.split('/')[1]
-    
+
     try:
         put(archive_path, '/tmp/{}'.format(fp))
         run('mkdir -p /data/web_static/releases/{}'.format(fp))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(fp, fp))
         run('rm /tmp/{}'.format(fp))
-        run('mv -f /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'\
-            .format(fp, fp))
+        run('mv -f /data/web_static/releases/{}/web_static/* \
+        /data/web_static/releases/{}/'.format(fp, fp))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(fp))
         run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{} \
-            /data/web_static/current'.format(fp))
+        /data/web_static/current'.format(fp))
         print('New version deployed!')
         return True
 
