41e5e3ce56e087a14dff6d4065c15652bc69fd3d
vmdn23 <victormdnguyen88@gmail.com>
2018-08-15 10:34:50 +0000

    display C  followed by the value of the text variable

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays Hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbhb():
+    """Displays HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    """Displays C followed by the value of the text variable"""
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+if __name__ == "__main__":
+    app.run()
