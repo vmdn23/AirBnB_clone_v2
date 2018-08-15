7620fc3852114350f0c8999f91b1ad20bf1cda3a
vmdn23 <victormdnguyen88@gmail.com>
2018-08-15 10:13:26 +0000

    change syntax to single '

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,10 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """Displays Hello HBNB"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
     app.run()
