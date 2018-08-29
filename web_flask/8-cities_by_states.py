b0505dc413b941dae55e2565fa5302b802b9d0ec
vmdn23 <victormdnguyen88@gmail.com>
2018-08-29 21:36:41 +0000

    pep8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -17,6 +17,7 @@ def teardown(exception):
     """
     storage.close()
 
+
 @app.route('/states_list', strict_slashes=False)
 def display_html():
     """
@@ -24,6 +25,7 @@ def display_html():
     """
     states = storage.all("State")
     return render_template('7-states_list.html', states=states)
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
@@ -33,5 +35,6 @@ def cities_by_states():
     states = storage.all("State")
     return render_template('8-cities_by_states.html', states=states)
 
+
 if __name__ == "__main__":
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
